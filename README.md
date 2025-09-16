30edfe4ceefd6a6f54b345dc209cc3ba88735278
iwfd965 <yh9914@qq.com>
2025-09-16 15:06:33 +0800

    Replace upstream data source

---
+++ README.md
@@ -0,0 +1,6 @@
+# sing-geosite
+
+This is a **fork** of the original project [sing-geosite](https://github.com/SagerNet/sing-geosite) by nekohasekai <contact-sagernet@sekai.icu>.
+
+## Changes in this fork
+- Replace the upstream data source from [v2fly/domain-list-community](https://github.com/v2fly/domain-list-community) to [Loyalsoldier/v2ray-rules-dat](https://github.com/Loyalsoldier/v2ray-rules-dat)
--- main.go
+++ main.go
@@ -60,10 +60,10 @@ func get(downloadURL *string) ([]byte, error) {
 
 func download(release *github.RepositoryRelease) ([]byte, error) {
 	geositeAsset := common.Find(release.Assets, func(it *github.ReleaseAsset) bool {
-		return *it.Name == "dlc.dat"
+		return *it.Name == "geosite.dat"
 	})
 	geositeChecksumAsset := common.Find(release.Assets, func(it *github.ReleaseAsset) bool {
-		return *it.Name == "dlc.dat.sha256sum"
+		return *it.Name == "geosite.dat.sha256sum"
 	})
 	if geositeAsset == nil {
 		return nil, E.New("geosite asset not found in upstream release ", release.Name)
@@ -406,7 +406,7 @@ func release(source string, destination string, output string, cnOutput string, 
 	if err != nil {
 		log.Warn("missing destination latest release")
 	} else {
-		if os.Getenv("NO_SKIP") != "true" && strings.Contains(*destinationRelease.Name, *sourceRelease.Name) {
+		if os.Getenv("NO_SKIP") != "true" && strings.Contains(*destinationRelease.Name, *sourceRelease.TagName) {
 			log.Info("already latest")
 			setActionOutput("skip", "true")
 			return nil
@@ -416,14 +416,14 @@ func release(source string, destination string, output string, cnOutput string, 
 	if err != nil {
 		return err
 	}
-	setActionOutput("tag", *sourceRelease.Name)
+	setActionOutput("tag", *sourceRelease.TagName)
 	return nil
 }
 
 func main() {
 	err := release(
-		"v2fly/domain-list-community",
-		"sagernet/sing-geosite",
+		"Loyalsoldier/v2ray-rules-dat",
+		"iwfd965/sing-geosite",
 		"geosite.db",
 		"geosite-cn.db",
 		"rule-set",
